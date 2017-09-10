636962380a1e45c5cef15a2679dbbb9c121988a3
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-09-10 20:13:06 +0900

    Add spec

--- spec/lib/algorithm/enumerable_spec.rb
+++ spec/lib/algorithm/enumerable_spec.rb
@@ -3,6 +3,15 @@ RSpec.describe Algorithm do
     it "#sorted?" do
       list = [1, 2, 3]
       expect(list.sorted?).to eq(true)
+
+      list = [3, 2, 1]
+      expect(list.sorted?).to eq(false)
+    end
+
+    it "#sorted_by?" do
+      list = %w(1 2 13)
+      expect(list.sorted?).to eq(false)
+      expect(list.sorted_by?(&:to_i)).to eq(true)
     end
   end
 end
