0bae8fbea5a245f94df7d487cb14b1533d7fffbe
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-09-12 00:07:15 +0900

    Add UnionFind

--- lib/algorithm.rb
+++ lib/algorithm.rb
@@ -1,3 +1,4 @@
 require "algorithm/array"
 require "algorithm/enumerable"
 require "algorithm/priority_queue"
+require "algorithm/union_find"
---
+++ lib/algorithm/union_find.rb
@@ -0,0 +1,37 @@
+module Algorithm
+  class UnionFind
+    def initialize(n)
+      @size = Array.new(n, 1)
+      @parent = []
+
+      n.times do |i|
+        @parent[i] = i
+      end
+    end
+
+    def find(x)
+      if @parent[x] == x
+        x
+      else
+        @parent[x] = find(@parent[x])
+      end
+    end
+
+    def unite(x, y)
+      x = find(x)
+      y = find(y)
+      return if x == y
+
+      @parent[y] = x
+      @size[x] += @size[y]
+    end
+
+    def same(x, y)
+      find(x) == find(y)
+    end
+
+    def size(x)
+      @size[find(x)]
+    end
+  end
+end
