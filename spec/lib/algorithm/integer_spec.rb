e67d4479972e85af84ff68701844ecfb67d9f049
Shuichi Tamayose <tmshuichi@gmail.com>
2019-02-18 00:59:31 +0900

    Fix rspec style

--- spec/lib/algorithm/integer_spec.rb
+++ spec/lib/algorithm/integer_spec.rb
@@ -1,20 +1,20 @@
 RSpec.describe Algorithm do
-  context "Integer" do
-    it "#combination" do
-      expect(4.combination(1)).to eq(4)
-      expect(4.combination(0)).to eq(1)
-      expect(10.combination(5)).to eq(252)
-      expect(20.combination(5)).to eq(15504)
+  describe Integer do
+    describe '#combination' do
+      it { expect(4.combination(1)).to eq(4) }
+      it { expect(4.combination(0)).to eq(1) }
+      it { expect(10.combination(5)).to eq(252) }
+      it { expect(20.combination(5)).to eq(15504) }
     end
 
-    it "#factorial" do
-      expect(3.factorial).to eq(6)
-      expect(0.factorial).to eq(1)
+    describe '#factorial' do
+      it { expect(3.factorial).to eq(6) }
+      it { expect(0.factorial).to eq(1) }
     end
 
-    it "#permutation" do
-      expect(3.permutation(2)).to eq(6)
-      expect(20.permutation(5)).to eq(1860480)
+    describe '#permutation' do
+      it { expect(3.permutation(2)).to eq(6) }
+      it { expect(20.permutation(5)).to eq(1860480) }
     end
   end
 end
