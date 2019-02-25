e0b0087414cb9c0ff639d182e7dfce17970504a4
Shuichi Tamayose <tmshuichi@gmail.com>
2019-02-25 22:45:15 +0900

    Fix bug

--- lib/algorithm/array.rb
+++ lib/algorithm/array.rb
@@ -1,17 +1,18 @@
 class Array
   def warshall_floyd
     v = size
-    ret = Array.new(v) { Array.new(v, Float::INFINITY) }
+
+    dist = map(&:dup)
 
     v.times do |k|
       v.times do |i|
         v.times do |j|
-          ret[i][j] = [ret[i][j], self[i][k] + self[k][j]].min
+          dist[i][j] = [dist[i][j], dist[i][k] + dist[k][j]].min
         end
       end
     end
 
-    ret
+    dist
   end
 
   # Longest Increasing Subsequence
