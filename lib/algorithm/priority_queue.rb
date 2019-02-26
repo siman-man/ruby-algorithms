7f80f89e37d2e7b950ce0a918578e784f1d912f7
Shuichi Tamayose <tmshuichi@gmail.com>
2019-02-26 15:27:49 +0900

    Add FastPriorityQueue

--- lib/algorithm/priority_queue.rb
+++ lib/algorithm/priority_queue.rb
@@ -75,4 +75,63 @@ module Algorithm
       @nodes.first
     end
   end
+
+  class FastPriorityQueue
+    attr_reader :size
+
+    def initialize(order: :asc)
+      @size = 0
+      @nodes = []
+    end
+
+    def empty?
+      @size.zero?
+    end
+
+    def push(node)
+      index = size
+      @size += 1
+
+      while index > 0
+        parent = (index - 1) / 2
+
+        break if @nodes[parent].last <= node.last
+
+        @nodes[index] = @nodes[parent]
+        index = parent
+      end
+
+      @nodes[index] = node
+    end
+
+    alias_method :<<, :push
+
+    def pop
+      node = @nodes.first
+      root = @nodes[size - 1]
+      @size -= 1
+      index = 0
+
+      while index * 2 + 1 < @size
+        left = 2 * index + 1
+        right = left + 1
+
+        if right < size && @nodes[right].last < @nodes[left].last
+          left = right
+        end
+
+        break if @nodes[left].last >= root.last
+
+        @nodes[index] = @nodes[left]
+        index = left
+      end
+
+      @nodes[index] = root
+      node.first
+    end
+
+    def top
+      @nodes.first
+    end
+  end
 end
