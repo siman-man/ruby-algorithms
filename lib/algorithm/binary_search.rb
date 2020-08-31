b46ddad6fc193228ee9906fffdf81d737348c2e9
Shuichi Tamayose <tmshuichi@gmail.com>
2020-08-31 20:54:27 +0900

    Add Range#bsearch

--- lib/algorithm/binary_search.rb
+++ lib/algorithm/binary_search.rb
@@ -1,3 +1,22 @@
+class Range
+  def bsearch
+    ng = first
+    ok = last
+
+    while (ok - ng).abs > 1
+      mid = (ok + ng) / 2
+
+      if yield(mid)
+        ok = mid
+      else
+        ng = mid
+      end
+    end
+
+    ok
+  end
+end
+
 def binary_search(ok:, ng:)
   while (ok - ng).abs > 1
     mid = (ok + ng) / 2
